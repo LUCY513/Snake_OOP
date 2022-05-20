b68aea1c1c3776eb842564223a444883c7b69d45
LUCY <Lucy@example.com>
2022-05-20 17:28:28 +0300

    Add stop - readkey

---
+++ Snake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Snake_OOP
+{
+    class Snake
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+
+
+
+
+
+            dfgdfgffdg
+        }
+    }
+}
